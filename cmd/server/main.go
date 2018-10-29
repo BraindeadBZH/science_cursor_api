b4b0c6d654ee776433d2b0ce7e074a4e38837a87
BraindeadBZH <braindeadbzh@gmail.com>
2018-10-29 12:19:36 +0100

    Initial commit

---
+++ cmd/server/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	sc "github.com/BraindeadBZH/science_cursor_api"
+	"os"
+)
+
+func main() {
+	sc.Test()
+	os.Exit(0)
+}
---
+++ science_cursor.go
@@ -0,0 +1,10 @@
+package api
+
+import (
+	"fmt"
+)
+
+// Test is just an hello world function
+func Test() {
+	fmt.Println("Hello World!!!")
+}
